167e715155877777eef19c05023ac66c07eb9b78
Artur Bartkowicz <artur.bartkowicz1@gmail.com>
2022-06-25 15:37:12 +0200

    Refactore application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,10 +3,10 @@ class ApplicationController < ActionController::API
   rescue_from ActionController::ParameterMissing, with: :parameter_missing
 
   def resource_not_found
-    render json: {"error": "Resource not found"}, status: :not_found
+    render json: {error: "Resource not found"}, status: :not_found
   end
 
   def parameter_missing
-    render json: {"error": "You must provide a parameter for this request"}, status: :not_found
+    render json: {error: "You must provide a parameter for this request"}, status: :not_found
   end
 end
